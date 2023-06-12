bfcef151186f324c043541f047b83ba425f89af2
muhammednadeerrahman <mohdnadeerrahman@gmail.com>
2023-06-12 16:43:31 +0530

    security page is ready

--- src/components/screens/Security.js
+++ src/components/screens/Security.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import Charted from './Charted'
 import styled from 'styled-components'
+import Header from '../includes/Header'
+import { Container } from '@mui/material'
 
 export default function Security() {
   return (
     <>
+       
         <SecurityPage>
-           <h1>Coming soon..!</h1>
+            <Header/>
+            <Content>
+              <Title>Coming soon..!</Title>
+            </Content>
+           
         </SecurityPage>
     </>
   )
 }
 
 const SecurityPage = styled.div`
-width:100vh;
+height:100vh;
+width: 100%;
+background-color: #111219;
+align-items: center;
+
 `
+const Content = styled.div`
+display: flex;
+justify-content: center;
+align-items:center;
+width: 100%;
+height: 80%;
+`
+
+const Title = styled.h1`
+text-align: center;
+font-weight: 700;
+font-size: 42px;
+color:#BDC5D9;
+`
+
+
  
