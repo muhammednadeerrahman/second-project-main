60c75590ed1d2c8b4dbafd5b6d0dc64f9ac440de
muhammednadeerrahman <mohdnadeerrahman@gmail.com>
2023-06-12 16:30:18 +0530

    graph 1h data finished

--- src/components/screens/Security.js
+++ src/components/screens/Security.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Charted from './Charted'
+import styled from 'styled-components'
 
 export default function Security() {
   return (
     <>
-        <h1>Coming soon..!</h1>
-       
+        <SecurityPage>
+           <h1>Coming soon..!</h1>
+        </SecurityPage>
     </>
   )
 }
+
+const SecurityPage = styled.div`
+width:100vh;
+`
  
